d0fe6548b8deac296777268f45d3d9e89058840e
Brian Hechinger <wonko@4amlunch.net>
2024-02-14 17:39:34 +0000

    fixed flake stuff

--- default.nix
+++ default.nix
@@ -1,48 +1,37 @@
-{
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+{stdenv, ncurses, pipewire, libprom, libpromhttp, libmicrohttpd} :
+stdenv.mkDerivation {
+  name = "pipewire-exporter";
+  #nativeBuildInputs = [
+  #  pkgs.cmake
+  #];
 
-  outputs = { self, nixpkgs }: {
+  buildInputs = [
+    ncurses
+    pipewire
+    libprom
+    libpromhttp
+    libmicrohttpd
+  ];
 
-    defaultPackage.x86_64-linux =
-      # Notice the reference to nixpkgs here.
-      with import nixpkgs { system = "x86_64-linux"; };
+  src = ./src;
 
-      stdenv.mkDerivation {
-        name = "pipewire-exporter";
-        #nativeBuildInputs = [
-        #  pkgs.cmake
-        #];
+  # Use $CC as it allows for stdenv to reference the correct C compiler
+  buildPhase = ''
+    $CC pw-exporter.c \
+      -I ${pipewire.dev}/include/spa-0.2 \
+      -I ${pipewire.dev}/include/pipewire-0.3 \
+      -L ${pipewire.dev}/lib \
+      -lncurses \
+      -lpipewire-0.3 \
+      -lprom \
+      -lpromhttp \
+      -lmicrohttpd \
+      -g \
+      -o pw-exporter
+  '';
 
-        buildInputs = [
-          ncurses
-          pipewire
-          libprom
-          libpromhttp
-          libmicrohttpd
-        ];
-
-        src = ./src;
-
-        # Use $CC as it allows for stdenv to reference the correct C compiler
-        buildPhase = ''
-          $CC pw-exporter.c \
-            -I ${pipewire.dev}/include/spa-0.2 \
-            -I ${pipewire.dev}/include/pipewire-0.3 \
-            -L ${pipewire.dev}/lib \
-            -lncurses \
-            -lpipewire-0.3 \
-            -lprom \
-            -lpromhttp \
-            -lmicrohttpd \
-            -g \
-            -o pw-exporter
-        '';
-
-        installPhase = ''
-          mkdir -p $out/bin
-          cp pw-exporter  $out/bin/pw-exporter
-        '';
-      };
-
-  };
+  installPhase = ''
+    mkdir -p $out/bin
+    cp pw-exporter  $out/bin/pw-exporter
+  '';
 }
--- shell.nix
+++ shell.nix
@@ -1,9 +1,9 @@
-{ pkgs }:
-pkgs.mkShell {
+{ mkShell, clang, libclang }:
+mkShell {
   # Get dependencies from the main package
   #inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   # Additional tooling
-  buildInputs = with pkgs; [
+  buildInputs = [
     clang
     libclang
   ];
