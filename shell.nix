bce94dcdf919c409c6204a3130ec65de2a86a6a5
Brian Hechinger <wonko@4amlunch.net>
2024-02-14 17:00:33 +0000

    reverted this

--- shell.nix
+++ shell.nix
@@ -1,13 +1,10 @@
-{ pkgs }:
+{ pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   # Get dependencies from the main package
-  #inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
+  inputsFrom = [ (pkgs.callPackage ./default.nix { }) ];
   # Additional tooling
   buildInputs = with pkgs; [
-    pkg-config
-    pipewire
     clang
     libclang
-    meson
   ];
 }
